ae405752d2de813ae00b6874ee5765d09e25ccec
Lucas Ferreira <ljnferreira@gmail.com>
2020-07-08 22:02:31 -0300

    FEAT: Added UI Layout component

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Main from '../Main'
+
+import { Container, Wrapper } from "./styles";
+
+export default function Layout() {
+  return (
+    <Container>
+      <Wrapper>
+        {/* <MenuBar/> */}
+        <Main/>
+        {/* <SideBar/> */}
+      </Wrapper>
+    </Container>
+  )
+}
