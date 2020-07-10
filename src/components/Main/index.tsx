c6754cdabbf5ba3727d8e30b408ab735433047fb
Lucas Ferreira <ljnferreira@gmail.com>
2020-07-09 21:54:46 -0300

    FEAT: Added header to the main component with styles

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { Container, Header, BackIcon, ProfileInfo } from "./styles";
+
+const Main: React.FC = () =>{
+  return (
+    <>
+      <Container>
+        <Header>
+          <button>
+            <BackIcon/>
+          </button>
+          <ProfileInfo>
+            <strong>
+              Lucas Ferreira
+            </strong>
+            <span>153 Tweets</span>
+          </ProfileInfo>
+        </Header>
+
+        {/* <ProfilePage/> */}
+
+        {/* <BottomMenu>
+          <HomeIcon />
+          <SearchIcon />
+          <BellIcon />
+          <EmailIcon />
+        </BottomMenu> */}
+
+      </Container>
+    </>
+  )
+}
+
+export default Main;
