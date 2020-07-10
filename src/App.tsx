35cfb749681045d9a5b42c49559bbdb0b015a541
Lucas Ferreira <ljnferreira@gmail.com>
2020-07-09 21:52:49 -0300

    FEAT: Added Layout component to view

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import GlobalStyles from './styles/GlobalStyles'
+import Layout from './components/Layout';
 
 function App() {
   return (
     <>
-      <h1>Lucas</h1>
+      <Layout/>
       <GlobalStyles/>
     </>
   );
