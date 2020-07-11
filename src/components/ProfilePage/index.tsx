13de1d2829c050f1bf25522afddcdb6359152252
Lucas Ferreira <ljnferreira@gmail.com>
2020-07-10 22:24:17 -0300

    FEAT: ProfilePage component created with basic infrmations, edit button will be added later

---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import { 
+  Container,
+  Banner,
+  Avatar,
+  ProfileData,
+  LocationIcon,
+  CakeIcon,
+  Followage 
+} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+      <Banner>
+        <Avatar />
+      </Banner>
+      <ProfileData>
+        {/* <EditButton outlined >Editar Perfil</EditButton> */}
+        
+        <h1>Lucas Ferreira</h1>
+        <h2>@ljnferreira</h2>
+
+        <p>
+          Developer at <a href="https://ljnferreira.com.br/">@ljnferreira</a>
+        </p>
+
+        <ul>
+          <li>
+            <LocationIcon />
+            Minas Gerais, Brasil
+          </li>
+          <li>
+            <CakeIcon />
+            Nascido(a) em 25 de agosto de 1997
+          </li>
+
+        </ul>
+
+        <Followage>
+          <span>
+            seguindo <strong>94</strong>
+          </span>
+          <span>
+            <strong>150 </strong> seguidores
+          </span>
+        </Followage>
+
+      </ProfileData>
+
+    </Container>
+  );
+}
+
+export default ProfilePage;
