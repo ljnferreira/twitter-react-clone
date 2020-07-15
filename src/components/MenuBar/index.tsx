b033572818bd083e95e5b3f8d204147fb7d9aab9
Lucas Ferreira <ljnferreira@gmail.com>
2020-07-14 22:45:16 -0300

    FEAT: Creating and adding the MenuBar component to Layout UI

---
+++ src/components/MenuBar/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import { 
+  Container,
+  TopSide,
+  Logo,
+  MenuButton,
+  HomeIcon,
+  BellIcon,
+  EmailIcon,
+  FavoriteIcon, 
+  ProfileIcon, 
+  FeatherIcon
+} from './styles';
+
+import Button from '../Button';
+
+const MenuBar: React.FC = () => {
+  return (
+    <Container>
+      <TopSide>
+        <Logo />
+
+        <MenuButton>
+          <HomeIcon />
+          <span>Pagina Inicial</span>
+        </MenuButton>
+        
+        <MenuButton>
+          <BellIcon />
+          <span>Notificações</span>
+        </MenuButton>
+
+        <MenuButton>
+          <EmailIcon />
+          <span>Mensagens</span>
+        </MenuButton>
+
+        <MenuButton>
+          <FavoriteIcon />
+          <span>Favoritados</span>
+        </MenuButton>
+
+        <MenuButton className="active">
+          <ProfileIcon />
+          <span>Perfil</span>
+        </MenuButton>
+
+        <Button>
+          <span>Tweetar</span>
+          <FeatherIcon/>
+        </Button>
+
+      </TopSide>
+    </Container>
+  );
+}
+
+export default MenuBar;
