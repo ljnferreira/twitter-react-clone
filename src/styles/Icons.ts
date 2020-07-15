9fce1e9879fd7eb70511b8e750304e8594df8fc2
Lucas Ferreira <ljnferreira@gmail.com>
2020-07-14 22:43:18 -0300

    UP: Adding Feather icon to Project

--- src/styles/Icons.ts
+++ src/styles/Icons.ts
@@ -10,5 +10,6 @@ export {
 export { Person, Cake, Favorite } from 'styled-icons/material';
 export { Chat } from 'styled-icons/heroicons-outline';
 export { ArrowLeft } from 'styled-icons/heroicons-solid';
+export { Feather } from '@styled-icons/entypo/Feather';
 export { Retweet } from '@styled-icons/fa-solid/Retweet';
 export { Twitter } from '@styled-icons/fa-brands/Twitter'
