a210575e55f2c3c4446fe56e8ace763766350f28
Lucas Ferreira <ljnferreira@gmail.com>
2020-07-20 21:09:16 -0300

    UP: Added StickyBox from react-stick-box library to emulate sidebar scroll from original Twitter

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import StickBox from 'react-sticky-box';
+
 
 import {
   Container,
@@ -19,41 +21,78 @@ const SideBar: React.FC = () => {
         <SearchInput placeholder="Buscar no Twitter"/>
         <SearchIcon/>
       </SearchWrapper>
-      <Body>
-        <List
-          title="Talvez você curta"
-          elements={[
-            <FollowSugestion
-              name="Fred Barbosa"
-              nickname="@freddybarbosa"
-            />,
-            <FollowSugestion
-              name="Arthur Assunção"
-              nickname="@arthurassunção" 
-            />,
-            <FollowSugestion
-              name="Mateus Ferreira"
-              nickname="@mtsferreirasilva" 
-            /> ]}
-        />
 
-        <List
-          title="Talvez você curta"
-          elements={[
-            <News 
-              place="Brasil"
-              title="Vacina de Oxford obtem sucesso..."
-            />,
-            <News
-              place="Mundo"
-              title="Eleições presidenciais americanas..."/>,
-            <News 
-              place="Brasil"
-              title="Médico é o profissional que o brasileiro mais confia"
-            />
-          ]}
-        />
-      </Body>
+      <StickBox>
+        <Body>
+          <List
+            title="Talvez você curta"
+            elements={[
+              <FollowSugestion
+                name="Fred Barbosa"
+                nickname="@freddybarbosa"
+              />,
+              <FollowSugestion
+                name="Arthur Assunção"
+                nickname="@arthurassunção" 
+              />,
+              <FollowSugestion
+                name="Mateus Ferreira"
+                nickname="@mtsferreirasilva" 
+              /> ]}
+          />
+
+          <List
+            title="Talvez você curta"
+            elements={[
+              <News 
+                place="Brasil"
+                title="Vacina de Oxford obtem sucesso..."
+              />,
+              <News
+                place="Mundo"
+                title="Eleições presidenciais americanas..."/>,
+              <News 
+                place="Brasil"
+                title="Médico é o profissional que o brasileiro mais confia"
+              />
+            ]}
+          />
+          <List
+            title="Talvez você curta"
+            elements={[
+              <FollowSugestion
+                name="Fred Barbosa"
+                nickname="@freddybarbosa"
+              />,
+              <FollowSugestion
+                name="Arthur Assunção"
+                nickname="@arthurassunção" 
+              />,
+              <FollowSugestion
+                name="Mateus Ferreira"
+                nickname="@mtsferreirasilva" 
+              /> ]}
+          />
+
+          <List
+            title="Talvez você curta"
+            elements={[
+              <News 
+                place="Brasil"
+                title="Vacina de Oxford obtem sucesso..."
+              />,
+              <News
+                place="Mundo"
+                title="Eleições presidenciais americanas..."/>,
+              <News 
+                place="Brasil"
+                title="Médico é o profissional que o brasileiro mais confia"
+              />
+            ]}
+          />
+        </Body>
+      </StickBox>
+      
     </Container>
   );
 }
