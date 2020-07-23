6144bb2fa5f9afe56b219f28900fbba25813d11e
Lucas Ferreira <ljnferreira@gmail.com>
2020-07-22 22:04:13 -0300

    Creating Api Services and an example of how to create the enviroment file with needed keys

---
+++ src/services/api.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const bearer = process.env.REACT_APP_BEARER_TOKEN;
+const url = process.env.REACT_APP_TWITTER_URL;
+
+console.log(bearer);
+
+const api = axios.create({
+  baseURL: url,
+  headers: {'Authoization' : 'bearer '+bearer}
+});
+
+export default api;
